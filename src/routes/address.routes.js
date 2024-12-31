64ab056d2954657a1e71303ce8c6b2aef67501a6
Vedant005 <vedantkanekar909@gmail.com>
2024-12-31 17:34:14 +0530

    refactor: implemented routes for address

---
+++ src/routes/address.routes.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import {
+  getAllAddresses,
+  createAddress,
+  updateAddress,
+  deleteAddress,
+} from "../controllers/address.controller.js";
+
+const router = express.Router();
+
+// Require authentication for all routes
+router.use(verifyJWT);
+
+router.get("/get", getAllAddresses); // Get all addresses
+router.post("/create", createAddress); // Create a new address
+router.put("/:addressId", updateAddress); // Update an address
+router.delete("/:addressId", deleteAddress); // Delete an address
+
+export default router;
