38da9fa6f37e63971be65aa0abc1d7e7399266f9
Vedant005 <vedantkanekar909@gmail.com>
2025-01-11 16:09:46 +0530

    refactor: replace cron with node-cron for improved scheduling

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import cron from "cron";
+import cron from "node-cron";
+
 import axios from "axios";
 
 const app = express();
